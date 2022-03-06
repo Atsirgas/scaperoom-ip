63776a25254fb3ddfc002dc6916f269172c0d0f2
iker <100007043.joan23@fje.edu>
2022-03-06 20:09:00 +0100

    06/03/2022 if enunciado1 y despedida

---
+++ interaccionesLayton/respuesta1.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($respu) && $respu=="si") echo "checked";
+if (isset($respu) && $respu=="no") echo "checked";
+
+$respu=$_POST['respu'];
+if($respu=="si"){
+    header ('Location: ./paginas/enunciado1.php');
+}
+     else{
+        header ('Location: ./despedida.html');
+     }
+?>
